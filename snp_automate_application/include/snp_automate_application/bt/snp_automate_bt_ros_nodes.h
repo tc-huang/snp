d16916b4c5562176ab089d6ae04e167e131beb38
Yolnan <yolnantiger@gmail.com>
2024-03-22 11:39:15 -0500

    implement behavior tree for demo and remove commented code

--- snp_automate_application/include/snp_automate_application/bt/snp_automate_bt_ros_nodes.h
+++ snp_automate_application/include/snp_automate_application/bt/snp_automate_bt_ros_nodes.h
@@ -6,40 +6,8 @@
 #include <motoros2_interfaces/srv/start_traj_mode.hpp>
 #include <motoros2_interfaces/srv/start_point_queue_mode.hpp>
 
-static const std::string CONTROLLER_JOINT_NAMES_PARAM = "controller_joint_names";
-
 namespace snp_automate_application
 {
-// class WriteSingleIONode : public snp_application::SnpRosServiceNode<motoros2_interfaces::srv::WriteSingleIO>
-// {
-// public:
-//   inline static std::string ADDRESS_KEY = "address";
-//   inline static std::string VALUE_KEY = "value";
-//   static BT::PortsList providedPorts()
-//   {
-//     return providedBasicPorts({BT::InputPort(ADDRESS_KEY), BT::InputPort(VALUE_KEY)});
-//   }
-//   using snp_application::SnpRosServiceNode<motoros2_interfaces::srv::WriteSingleIO>::SnpRosServiceNode;
-
-//   bool setRequest(typename Request::SharedPtr& request) override;
-//   BT::NodeStatus onResponseReceived(const typename Response::SharedPtr& response) override;
-// };
-
-// class ReadSingleIONode : public snp_application::SnpRosServiceNode<motoros2_interfaces::srv::ReadSingleIO>
-// {
-// public:
-//   inline static std::string ADDRESS_KEY = "address";
-//   inline static std::string VALUE_KEY = "value";
-//   static BT::PortsList providedPorts()
-//   {
-//     return providedBasicPorts({BT::InputPort(ADDRESS_KEY), BT::OutputPort(VALUE_KEY)});
-//   }
-//   using snp_application::SnpRosServiceNode<motoros2_interfaces::srv::ReadSingleIO>::SnpRosServiceNode;
-
-//   bool setRequest(typename Request::SharedPtr& request) override;
-//   BT::NodeStatus onResponseReceived(const typename Response::SharedPtr& response) override;
-// };
-
 class StartTrajModeNode : public snp_application::SnpRosServiceNode<motoros2_interfaces::srv::StartTrajMode>
 {
 public:
@@ -59,20 +27,5 @@ public:
   bool setRequest(typename Request::SharedPtr& request) override;
   BT::NodeStatus onResponseReceived(const typename Response::SharedPtr& response) override;
 };
-
-// class AddJointsToTrajectoryNode : public BT::RosTopicSubNode<sensor_msgs::msg::JointState>
-// {
-// public:
-//   inline static std::string TRAJECTORY_INPUT_PORT_KEY = "input";
-//   inline static std::string TRAJECTORY_OUTPUT_PORT_KEY = "output";
-//   inline static BT::PortsList providedPorts()
-//   {
-//     return providedBasicPorts({ BT::InputPort<trajectory_msgs::msg::JointTrajectory>(TRAJECTORY_INPUT_PORT_KEY),
-//                                BT::OutputPort<trajectory_msgs::msg::JointTrajectory>(TRAJECTORY_OUTPUT_PORT_KEY) });
-//   }
-//   using BT::RosTopicSubNode<sensor_msgs::msg::JointState>::RosTopicSubNode;
-
-//   BT::NodeStatus onTick(const typename sensor_msgs::msg::JointState::SharedPtr& last_msg) override;
-// };
 
 }  // namespace snp_automate_application
--- snp_automate_application/scripts/motoros2_simulator.py
+++ snp_automate_application/scripts/motoros2_simulator.py
@@ -2,7 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
-from motoros2_interfaces.srv import StartTrajMode, ReadSingleIO, WriteSingleIO
+from motoros2_interfaces.srv import StartTrajMode, ReadSingleIO, WriteSingleIO, StartPointQueueMode
 from motoros2_interfaces.msg import MotionReadyEnum
 from std_srvs.srv import Trigger
 
@@ -12,8 +12,7 @@ class RobotServiceNode(Node):
 
         self.create_service(StartTrajMode, 'start_traj_mode', self.enable_robot_callback)
         self.create_service(Trigger, 'stop_traj_mode', self.disable_robot_callback)
-        self.create_service(ReadSingleIO, 'read_single_io', self.read_single_io_callback)
-        self.create_service(WriteSingleIO, 'write_single_io', self.write_single_io_callback)
+        self.create_service(StartPointQueueMode, 'start_point_queue_mode', self.enable_robot_callback)
         self.io = {}
 
     async def enable_robot_callback(self, request, response):
@@ -27,23 +26,6 @@ class RobotServiceNode(Node):
         response.message = 'Robot disabled'
         response.success = True
         return response
-
-    async def read_single_io_callback(self, request, response):
-        if request.address in self.io:
-            response.value = self.io[request.address]
-            response.success = True
-            response.message = f'Set IO {request.address} to {response.value}'
-        else:
-            response.message = f'Address {request.address} does not exist'
-            response.success = False
-        return response
-
-    async def write_single_io_callback(self, request, response):
-        self.io[request.address] = request.value
-        response.success = True
-        response.message = f'Successfully wrote IO {request.address} to {request.value}'
-        return response
-
 
 def main(args=None):
     rclpy.init(args=args)
